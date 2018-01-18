3627ef7c29671a46fdacabb09c275940b050f04e
Yogeshwar Pullagurla <yogeshwar.pullagurla@accoliteindia.com>
2018-01-18 13:46:26 +0530

    added iifes and more refactoring

--- ex1.js
+++ ex1.js
@@ -1,80 +1,81 @@
-A();
+(function () {
+	A();
 
-function C () {
-	console.log("OOPS!");
-}
+	function C () {
+		console.log("OOPS!");
+	}
 
-function E (f) {
-	console.log("E");
-	f();
-	var f = F;
-}
+	function E (f) {
+		console.log("E");
+		f();
+		var f = F;
+	}
 
-function A () {
-	console.log("A");
-	B();
-};
+	function A () {
+		console.log("A");
+		B();
+	}
 
-var C;
+	var C;
 
-function G () {
-	console.log("G");
-	H();
+	function G () {
+		console.log("G");
+		H();
 
-	function H () {
-		console.log("H");
-		I();
+		function H () {
+			console.log("H");
+			I();
+		}
+	}
+
+	var D = d;
+
+	function d () {
+		console.log("D");
+		E(F);
+	}
+
+	function I () {
+		console.log("I");
+		J();
+		J();
+	}
+
+	function B () {
+		console.log("B");
+		C();
 	};
-}
 
-var D = d;
+	function F () {
+		console.log("F");
+		G();
+	};
 
-function d () {
-	console.log("D");
-	E(F);
-}
+	var rest = "KLMNOPQRSTUVWXYZ".split("");
+	for (var i = 0; i < rest.length; i++) {
+		(function (i) {
+			// define the current function
+			window[rest[i]] = function () {
+				console.log(rest[i]);
+				if (i < (rest.length - 1)) {
+					window[rest[i + 1]]();
+				}
+			};
+		})(i);
+	}
 
-function I () {
-	console.log("I");
-	J();
-	J();
-}
-
-function B () {
-	console.log("B");
-	C();
-};
-
-function F () {
-	console.log("F");
-	G();
-};
-
-var rest = "KLMNOPQRSTUVWXYZ".split("");
-for (var i = 0; i < rest.length; i++) {
-	(function (i) {
-		// define the current function
-		window[rest[i]] = function () {
-			console.log(rest[i]);
-			if (i < (rest.length - 1)) {
-				window[rest[i + 1]]();
-			}
+	function J () {
+		J = function () {
+			console.log("J");
+			setTimeout(() => {
+				K();
+			});
 		};
-	})(i);
-}
-
-
-function J () {
-	J = function () {
-		console.log("J");
 	};
-	setTimeout(() => {
-		K();
-	});
-};
 
-function C () {
-	console.log("C");
-	d();
-};
+	function C () {
+		console.log("C");
+		d();
+	}
 
+})();
